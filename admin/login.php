87317205ec45d33ab78c2ca8c1c0d7f6284f61ee
SliinQ <sliinq2012@gmail.com>
2016-03-21 21:04:54 +0100

    ...

--- admin/login.php
+++ admin/login.php
@@ -7,8 +7,10 @@
     if (isset($_POST['username']) && isset($_POST['password'])) {
         if (!empty($_POST['username']) && !empty($_POST['password'])) {
             $user = 'Admin';
-            $pass = '$2y$10$JUITbVJIU1ecjeQGtE2QgeasKcoYDPHrL9SoDHULE0U0UcoAbYNSW';
-            if ($_POST['username'] == $user && password_verify($_POST['password'], $pass)) {
+            // $pass = '$2y$10$JUITbVJIU1ecjeQGtE2QgeasKcoYDPHrL9SoDHULE0U0UcoAbYNSW';
+            $pass = '6c7ce59b47bb77417d38e9a8e488d1a2f5312881';
+            // if ($_POST['username'] == $user && password_verify($_POST['password'], $pass)) {
+            if ($_POST['username'] == $user && sha1($_POST['password']) == $pass) {
                 session_start();
                 $_SESSION['logged'] = true;
                 header("Location:index.php");
